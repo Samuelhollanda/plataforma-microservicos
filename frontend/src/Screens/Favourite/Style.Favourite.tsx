db197d38228f21855b1eee893548104dfb4f9165
sfmaii <mairajaciele616@gmail.com>
2026-04-27 11:14:39 -0300

    tela de favoritos

---
+++ frontend/src/Screens/Favourite/Style.Favourite.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create ({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor:  '#FC7A7A'
+    }, 
+    titulo: {
+        fontSize: 20, 
+        fontWeight: 'bold', 
+    },
+    buttonback:{
+        padding: 10,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        marginTop: 10,
+        alignItems: 'center'
+    },
+    textButton: {
+        fontWeight: 'bold', 
+    },
+    card: {
+        backgroundColor: '#fff',
+        padding: 15,
+        marginVertical: 10,
+        borderRadius: 8,
+
+    }, 
+    text: {
+        fontSize: 16, 
+    }
+});
