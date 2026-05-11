7cabec319dba0432e3d61df71a29a2c15b09c68c
PnP-01 <Ph316436@gmail.com>
2026-05-11 10:14:20 -0300

    separando a lista de contatos em um componente isolado

---
+++ frontend/src/components/ListaContatos/ListaContatos.tsx
@@ -0,0 +1,33 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { FlatList, Image, Text, TouchableOpacity } from 'react-native';
+import estilos from '../../Screens/Chat/Style.Chat';
+export default function ListaContatos({ dados }: { dados: any }) {
+    
+    const navigation = useNavigation<any>();
+
+    const Contato = ({ item }: any) => {
+        const imagemCerta =
+            typeof item.foto === 'string'
+                ? { uri: item.foto }
+                : item.foto;
+
+        return (
+            <TouchableOpacity 
+                style={estilos.itemLista}   
+                onPress={() => navigation.navigate('ChatDetail')}
+            >
+                <Image source={imagemCerta} style={estilos.fotoContato} />
+                <Text style={estilos.nomeContato}>{item.nome}</Text>
+            </TouchableOpacity>
+        );
+    };
+
+    return (
+        <FlatList
+            data={dados}
+            renderItem={Contato}
+            keyExtractor={(item) => item.id}
+        />
+    );
+}
